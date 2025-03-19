6f6f397c82fdf17394733c676f2500f8ec2e7425
Benjamin-Meyer-Dev <benjamin.meyer.cloud@gmail.com>
2025-03-18 22:41:03 -0400

    Fixing case issues

--- src/constants.py
+++ src/constants.py
@@ -19,12 +19,12 @@ TITLE_BAR_HEIGHT = 40
 ENTER_KEYCODE = 16777220
 WEBCAM_UPDATE = 60
 
-DATABASE_PATH = "../Database/sign_language.db"
-EXIT_ICON_PATH = "../Images/exit_icon.svg"
-GUI_STYLING_PATH = "./QSS/gui.qss"
-LETTER_EXAMPELS_PATH = "../Images/Letter Examples"
-MODEL_PATH = "../Model/sign_language_model.keras"
-PROGRAM_ICON_PATH = "../Images/program_icon.svg"
+DATABASE_PATH = "../database/sign_language.db"
+EXIT_ICON_PATH = "../images/exit_icon.svg"
+GUI_STYLING_PATH = "./qss/gui.qss"
+LETTER_EXAMPELS_PATH = "../images/Letter Examples"
+MODEL_PATH = "../model/sign_language_model.keras"
+PROGRAM_ICON_PATH = "../images/program_icon.svg"
 
 LETTERS = {i: letter for i, letter in enumerate(string.ascii_uppercase[:12])}
 
